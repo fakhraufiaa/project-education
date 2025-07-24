ab4bbc98af25b88d0a096c51ebc3a370650057ab
Fakhra Aufia <fahrmnr@gmail.com>
2025-07-25 02:18:34 +0700

    update:route_category

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -3,11 +3,10 @@ import { PrismaClient } from "@/lib/generated/prisma";
 
 const prisma = new PrismaClient();
 
-export async function DELETE(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const categoryId = parseInt(params.id);
+export async function DELETE(req: Request) {
+  const url = new URL(req.url);
+  const id = url.pathname.split("/").pop(); // ambil ID dari URL
+  const categoryId = parseInt(id || "");
 
   if (isNaN(categoryId)) {
     return NextResponse.json({ message: "ID tidak valid" }, { status: 400 });
